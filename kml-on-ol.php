0d96ac160ab607e802d5615a98a0bd6be3d79c39
Nux <eccenux+git@gmail.com>
2022-09-28 21:16:13 +0200

    html5 starter

--- kml-on-ol.php
+++ kml-on-ol.php
@@ -1,7 +1,8 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE HTML>
 <html>
 	<head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Wikipedia on OpenStreetMap</title>
 
 <?php
