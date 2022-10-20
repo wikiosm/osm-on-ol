dc04f903d09a5759f94d38e03ed530644b173b2a
Nux <eccenux+git@gmail.com>
2022-10-20 02:37:04 +0200

    pl

---
+++ Lang/pl.js
@@ -0,0 +1,31 @@
+/* Copyright (c) 2006-2008 MetaCarta, Inc., published under the Clear BSD
+ * license.  See http://svn.openlayers.org/trunk/openlayers/license.txt for the
+ * full text of the license. */
+
+/* Translators:
+ *  - Maciej Nux Jaros
+ */
+
+/**
+ * @requires OpenLayers/Lang.js
+ */
+
+/**
+ * Namespace: OpenLayers.Lang["pl"]
+ * Dictionary for Polish.  Keys for entries are used in calls to
+ *     <OpenLayers.Lang.translate>.  Entry bodies are normal strings or
+ *     strings formatted for use with <OpenLayers.String.format> calls.
+ */
+OpenLayers.Lang["pl"] = OpenLayers.Util.applyDefaults({
+
+    'unhandledRequest': "Nieudane żądanie zwróciło: ${statusText}",
+
+    'permalink': "Link bezpośredni",
+
+    'overlays': "Warstwy dodatkowe",
+
+    'baseLayer': "Warstwa bazowa",
+
+    'scale': "Skala = 1 : ${scaleDenom}",
+    
+});
